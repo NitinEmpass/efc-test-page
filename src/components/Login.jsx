6698eab3b7fafeb19e9f271de956b17e3a18666d
NitinS87 <nitins9868@gmail.com>
2023-05-25 16:46:13 +0530

    Changed Image in login form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 // import techniques from "../assets/images/techniques.png";
 import LoginForm from "./LoginForm";
+import efc from "../assets/images/What-Is-Executive-Function.webp";
 
 const Login = () => {
   return (
@@ -15,13 +16,11 @@ const Login = () => {
       </div>
       <div className="flex flex-col lg:flex-row justify-center items-center mb-5 mx-auto gap-10 lg:gap-16">
         <div className="order-last lg:order-first">
-          <iframe
-            className="w-full lg:w-[500px] h-full lg:h-80 rounded-lg aspect-video"
-            src="https://www.youtube-nocookie.com/embed/FD_AdcTABWw?rel=0"
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowFullScreen
-          ></iframe>
+          <img
+            className="w-full lg:w-[600px] h-full lg:h-80 rounded-lg aspect-video"
+            src={efc}
+            alt="EFC"
+          />
         </div>
         <div className="border-orange-400 border-2 h-40 hidden lg:block"></div>
         <LoginForm />
